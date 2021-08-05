3f4d48aa1a1364a03507f223714fdfbf82206d96
akeverus <polaroids.of.tarkovsky@gmail.com>
2021-08-05 11:14:21 +0300

    Исходный файл ДЗ за 03.08.2021

---
+++ Обучение Java Developer/Занятия/02_Java.Уровень 2/src/lesson_6/Client.java
@@ -0,0 +1,7 @@
+package lesson_6;
+
+public class Client {
+    public static void main(String[] args) {
+    }
+}
+
---
+++ Обучение Java Developer/Занятия/02_Java.Уровень 2/src/lesson_6/Server.java
@@ -0,0 +1,7 @@
+package lesson_6;
+
+public class Server {
+
+    public static void main(String args[])  {
+    }
+}
